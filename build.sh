de209b0583d65f683930538e6ea32217c4d9d0bb
Johannes Schröder <contact@scribblerockerz.com>
2019-03-18 00:02:41 +0100

    Updated build script

--- build.sh
+++ build.sh
@@ -4,6 +4,11 @@
 # https://blog.codeship.com/building-minimal-docker-containers-for-go-applications/
 
 # build cryptletter executable
-go build -o bin/cryptletter src/*.go
+go build -o bin/cryptletter src/*.go && \
+echo "Successfully build executable to ./bin/cryptletter"
 
-echo "Successfully build executable to bin/cryptletter"
+npm run build 2>&1 1>/dev/null && \
+echo "Successfully build assets to ./public"
+
+echo "Finished build"
+
