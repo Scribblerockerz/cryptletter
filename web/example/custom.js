bf663304ed7cc0332840598ca0d24fb4ff1a0057
Johannes Schröder <jschroeder@uandi.com>
2021-05-01 21:56:57 +0200

    Add example for extendable locale configuration

---
+++ web/example/custom.js
@@ -0,0 +1,32 @@
+// Additional configuration
+window.cryptletterExtended = {
+    // change active locale
+    activeLocale: 'de',
+
+    // add additional locales or update existing ones
+    locales: {
+        de: {
+            slogan: 'Encrypted self-destructing messages.',
+            messagePlaceholder:
+                'Diesen Text entfernen und die entsprechende Nachricht hier eintragen.\n\nDie Nachricht wird verschlüsselt übertragen und ist nach der Erstellung nur einmalig aufrufbar.',
+            lifetimeHint:
+                'Dieser Cryptletter wird {selectDuration} nach dem ersten Aufruf unwiderruflich gelöscht.',
+            lifetimeHintAfter:
+                'Dieser Link ist nach dem erstmaligen Aufruf {duration} gültig.',
+            estimatedLifetimeHint:
+                'Dieser Cryptletter wird in {duration} unwiderruflich gelöscht.',
+            encryptMessageLabel: 'Nachricht verschlüsseln',
+            destroyMessageLabel: 'Nachricht jetzt löschen',
+            messageResult:
+                'Der folgende Link ist nach dem ersten Aufruf nur {duration} verfügbar.\n\n\n{url}\n',
+            messageEmpty: 'Looks like there was no message provided.',
+            decryptingMessage: 'Decrypting message, please wait ...',
+            missingMessageHeadline:
+                'Dieser Cryptletter ist nicht mehr verfügbar...',
+            missingMessageText:
+                'Wenn Sie hier stattdessen eine verschlüsselte Nachricht erwarten, kontaktieren Sie bitte Ihren Ansprechpartner.',
+            viewRawLabel: 'view raw',
+            viewTextLabel: 'view text',
+        },
+    },
+};
--- web/src/i18n/index.js
+++ web/src/i18n/index.js
@@ -1,10 +1,29 @@
 import { createI18n } from 'vue-i18n';
 import en from './en-messages.json';
 
+var activeLocale = 'en';
+const messages = {
+    en,
+};
+
+// Extendable locale configuration
+if (window.cryptletterExtended) {
+    // change active locale, at initialization
+    activeLocale = window.cryptletterExtended.activeLocale || activeLocale;
+
+    const extendedLocales = window.cryptletterExtended.locales || {};
+
+    Object.keys(extendedLocales).forEach((localeKey) => {
+        messages[localeKey] = messages[localeKey] || {};
+        messages[localeKey] = {
+            ...messages[localeKey],
+            ...extendedLocales[localeKey],
+        };
+    });
+}
+
 export default createI18n({
-    locale: 'en',
+    locale: activeLocale,
     legacy: false,
-    messages: {
-        en,
-    },
+    messages: messages,
 });
